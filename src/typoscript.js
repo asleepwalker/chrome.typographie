2579954b6aba4540c9a30889ffb1bf7485228030
asleepwalker <mail@asleepwalker.ru>
2014-07-13 18:50:59 +0300

    Added processing of text areas

--- src/typoscript.js
+++ src/typoscript.js
@@ -1,4 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	var active = document.activeElement;
-	//processing...
+	request = new XMLHttpRequest();
+	request.open('POST', 'http://api.typographie.ru/', true);
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400)
+			active.value = JSON.parse(request.responseText).result;
+	}
+
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	request.send('raw='+encodeURIComponent(active.value));
 });

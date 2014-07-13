7500d70b4566d4e504784a76672cff7b06623e06
asleepwalker <mail@asleepwalker.ru>
2014-07-13 12:38:12 +0300

    Added context menu listener

--- src/background.js
+++ src/background.js
@@ -1,10 +1,10 @@
-function typoprocess() {
-	chrome.runtime.sendMessage({}, function(response) {});
-}
-
 chrome.contextMenus.create({
 	"title" : "Typographie!",
 	"type" : "normal",
 	"contexts" : ["editable"],
-	"onclick" : typoprocess()
+	"onclick" : function() {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {});
+		});
+	}
 });

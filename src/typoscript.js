708b04e32b965e24995a685de5d4999b26f686da
Sleepwalker <mail@asleepwalker.ru>
2014-07-14 13:27:23 +0300

    Added contenteditable-fields support

--- src/typoscript.js
+++ src/typoscript.js
@@ -2,12 +2,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	var active = document.activeElement;
 	request = new XMLHttpRequest();
 	request.open('POST', 'http://api.typographie.ru/', true);
-
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400)
-			active.value = JSON.parse(request.responseText).result;
-	}
-
 	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send('raw='+encodeURIComponent(active.value));
+
+	if (!active.hasAttribute('contenteditable')) {
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400)
+				active.value = JSON.parse(request.responseText).result;
+		}
+		request.send('raw='+encodeURIComponent(active.value));
+	} else {
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400)
+				active.innerHTML = JSON.parse(request.responseText).result;
+		}
+		request.send('in=html&out=html&raw='+encodeURIComponent(active.innerHTML));
+	}
 });
